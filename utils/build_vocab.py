d29f349b901cc9e6b4a532eec27bc5e086db4002
episode <admin@episode.moe>
2025-12-12 16:47:12 +0800

    Update vocabulary building

--- utils/build_vocab.py
+++ utils/build_vocab.py
@@ -6,13 +6,13 @@ from datasets import load_from_disk
 # 路径配置
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.dirname(CURRENT_DIR)
-DATA_DIR = os.path.join(PROJECT_ROOT, "datasets", "processed")
+DATA_DIR = os.path.join(PROJECT_ROOT, "datasets", "mixed")
 
 
 def build_relation_vocab(dataset_name):
     dataset_path = os.path.join(DATA_DIR, dataset_name)
 
-    # 动态生成输出文件名: 例如 PQ_2h_relations.json
+    # 动态生成输出文件名: 例如 PQ_mix_relations.json
     output_filename = f"{dataset_name}_relations.json"
     output_path = os.path.join(dataset_path, output_filename)
 
@@ -58,11 +58,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Build relation vocab for a specific dataset")
     # 添加命令行参数 -d
     parser.add_argument('-d', '--dataset', type=str, required=True,
-                        help="Name of the dataset folder (e.g., PQ_2h)")
+                        help="Name of the dataset folder (e.g., PQ_mix)")
 
     args = parser.parse_args()
 
     build_relation_vocab(args.dataset)
 
 # 为特定数据集生成词表
-# python utils/build_vocab.py -d PQ_2h
+# python utils/build_vocab.py -d PQ_mix
